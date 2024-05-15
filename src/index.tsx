52266e4cc847f275b8f1d6031d7eda4beb6ba49c
vyazemskaya <vyazemskayanna@gmail.com>
2024-05-15 10:23:41 +0300

    add mobile styling

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./store/store";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -24,7 +24,4 @@ root.render(
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.register();
--- src/layout/header/header.component.tsx
+++ src/layout/header/header.component.tsx
@@ -1,43 +1,80 @@
-import type { Dispatch } from "react";
+import { type Dispatch, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import type { Action } from "redux";
 import Cart from "../../components/cart/cart.component";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { signOutStart } from "../../store/user/user.slice";
 import { HeaderProps } from "./header.props";
+import { ReactComponent as CloseIcon } from "./close-icon.svg";
+import { ReactComponent as HamburgerIcon } from "./hamburger-icon.svg";
 import styles from "./header.module.css";
+import cn from "classnames";
 
 export default function Header({ ...props }: HeaderProps) {
   const currentUser = useSelector(selectCurrentUser);
+  const location = useLocation();
   const dispatch: Dispatch<Action<"user/signOutStart">> = useDispatch();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    closeMenu();
+  }, [location]);
 
   function signOutUser() {
     dispatch(signOutStart());
   }
 
+  function clickHandler(e: React.MouseEvent) {
+    const target = e.target as Element;
+
+    if (!target.closest("#header-menu") || target.closest("#close-menu-btn")) {
+      closeMenu();
+    }
+  }
+
+  function closeMenu() {
+    document.documentElement.style.overflow = "auto";
+    setIsMenuOpen(false);
+  }
+
+  function openMenu() {
+    document.documentElement.style.overflow = "hidden";
+    setIsMenuOpen(true);
+  }
+
   return (
     <div {...props} className={styles.header}>
+      <button onClick={openMenu} className={styles.hamburger} type="button">
+        <HamburgerIcon />
+      </button>
       <Link to="/" className={styles.logo}>
         <span className={styles.heading}>depeche</span>
       </Link>
-      <nav className={styles.nav}>
-        <Link to="/shop" className={styles.navLink}>
-          shop all
-        </Link>
-        <Link to="/contacts" className={styles.navLink}>
-          contacts
-        </Link>
-        {currentUser ? (
-          <span className={styles.navLink} onClick={signOutUser}>
-            sign out
-          </span>
-        ) : (
-          <Link to="/login" className={styles.navLink}>
-            login
+      <div
+        onClick={clickHandler}
+        className={cn(styles.nav, {
+          [styles.isActive]: isMenuOpen,
+        })}
+      >
+        <button id="close-menu-btn" className={styles.close} type="button">
+          <CloseIcon />
+        </button>
+        <nav id="header-menu" className={styles.inner}>
+          <Link to="/shop" className={styles.navLink}>
+            shop all
           </Link>
-        )}
-      </nav>
+          {currentUser ? (
+            <span className={styles.navLink} onClick={signOutUser}>
+              sign out
+            </span>
+          ) : (
+            <Link to="/login" className={styles.navLink}>
+              login
+            </Link>
+          )}
+        </nav>
+      </div>
       <Cart />
     </div>
   );
